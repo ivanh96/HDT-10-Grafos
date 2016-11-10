02b677c71fe3e7b7ada88ff47b2c738ccc50630a
ivanh96 <ivanh96@hotmail.com>
2016-11-09 21:13:39 -0600

    Update Main.java

--- HojaGrafos/src/Main.java
+++ HojaGrafos/src/Main.java
@@ -26,7 +26,7 @@ public class Main {
 	
 	 public static void main(String[] args) {
 		
-		 File rute = new File("C:\\Users\\RodolfoC\\Documents\\Neo4j\\datos"); //CAMBIAR RUTA EN OTRA PC
+		 File rute = new File("/Users/MacBookPro/Documents/Neo4j/datos"); //CAMBIAR RUTA EN OTRA PC
 		 GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
 		 GraphDatabaseService dbService = dbFactory.newEmbeddedDatabase(rute);
 		 
@@ -392,9 +392,185 @@ public class Main {
 			 // A Persona_12
 			 Relationship P14_to_P12 = Persona_14.createRelationshipTo(Persona_12, RelationType.Mails);
 			 P14_to_P12.setProperty("Emails", 3);
-			 
+
+
+			 // Se hacen muestran todos los nodos y todas sus relaciones de emails
+             resultado1 = graphDb.execute("MATCH (P1:Persona)-[E:Emails]->(P2:Persona) RETURN P1.Nombre");
+             resultado2 = graphDb.execute("MATCH (P1:Persona)-[E:Emails]->(P2:Persona) RETURN P2.Nombre");
+             resultado3 = graphDb.execute("MATCH (P1:Persona)-[E:Emails]->(P2:Persona) RETURN E.Emails");
+
+             Iterator<String>r1 = resultado1.columnAs("P1.Nombre");
+             Iterator<String>r2 = resultado2.columnAs("P2.Nombre");
+             Iterator<String>r3 = resultado3.columnAs("E.Emails");
+            
+             // Se crean las listas para insertar los datos del iterador y se agregan los datos resultantes
+			 LinkedList<String> res1 = new LinkedList();
+             LinkedList<String> res2 = new LinkedList();
+             LinkedList<Object> res3 = new LinkedList();
+
+             while (r1.hasNext()){
+                res1.add(r1.next());
+             }
+             while (r2.hasNext()){
+                res2.add(r2.next());
+             }
+             while (r3.hasNext()){
+                res3.add(r3.next());
+             }
+          
+          	 // Se muestran la consulta de todos los correos enviados
+             int site1 = res1.site();
+             for(int i=0;i<site1;i++){
+                System.out.println("La "+res1.get(i)+" envio "+res3.get(i)+" emails a la "+res2.get(i));
+             }
+
+             // Se muestra las personas con minimo 6 emails
+             resultado4 = graphDb.execute("MATCH (P1:Persona)-[E:Emails]->(P2:Persona) WHERE C.Cantidad > 6 RETURN P1.Nombre");
+             resultado5 = graphDb.execute("MATCH (P1:Persona)-[E:Emails]->(P2:Persona) WHERE C.Cantidad > 6 RETURN P2.Nombre");
+             resultado6 = graphDb.execute("MATCH (P1:Persona)-[E:Emails]->(P2:Persona) WHERE C.Cantidad > 6 RETURN E.Emails");
+            
+             Iterator<String>r4=resultado4.columnAs("P1.Nombre");
+             Iterator<String>r5=resultado5.columnAs("P2.Nombre");
+             Iterator<String>r6=resultado6.columnAs("E.Emails");
+            
+             LinkedList<String> res4 = new LinkedList();
+             LinkedList<String> res5 = new LinkedList();
+             LinkedList<Object> res6 = new LinkedList();
+            
+             while (r4.hasNext()){
+             res4.add(r4.next());
+             }
+             while (r5.hasNext()){
+			 res5.add(r5.next());
+             }
+             while (r6.hasNext()){
+             res6.add(r6.next());
+             }
+           
+             int size1 = res4.size(); 
+             for(int i=0;i<size1;i++){
+             System.out.println("La "+res4.get(i)+" envio "+res6.get(i)+" emails a la "+res5.get(i));
+             }
+
+             // Se muestra las personas que mas se comunicaron y las que menos se comunicaron
+             int[] tabla= new int[14];
+             int[] orden= new int[tabla.length];
+             int k=-1;
+
+             for (int i=0; i<tabla.length; i++)
+			 tabla[i]=graphDb.getNodeById(i).getDegree(Direction.BOTH);
+
+             for (int i=0; i<tabla.length; i++){
+			 int b1=0;
+			 for(int j=0; j<tabla.length; j++){
+			 int d= tabla[j];
+             if (d>=b1){
+			 b1=d;
+			 k=j;
+             }
+			 }
+             orden[i]=k;
+             tabla[k]=0;
+             } 
+            
+             System.out.println("Las tabla muestra las personas más comunicadas a las menos comunicadas: ");
+
+             // Se imprime el nombre de las personas ya ordenado
+             for(int t=0; t<tabla.length; t++){
+			 System.out.println("Persona "+ (orden[t]+1));
+             }
+
+
+             // Se muestra la cantidad minima de emails enviados desde una persona origen
+             Vector<Integer> vectorID= new Vector<>();
+             Vector<Long> vectorEmails= new Vector<>();
+             for(int i=0; i<196; i++){
+             try{
+			 Relationship rel= graphDb.getRelationshipById(i);
+			 long emails = (long) rel.getProperty("Emails");
+			 vectorID.addElement(i);
+			 vectorEmails.addElement(emails); 
+			 }catch(Exception e){
+                    
+             }
+             }
+        
+             Scanner input= new Scanner(System.in);
+             int indNodo;
+             int finNodo;
+        
+             // Ingreso de la persona origen
+             System.out.println("Ingrese el ID de la persona origen: ");
+             indNodo = scan.nextInt() - 1;
+             int iNodo = indNodo;
+        
+             // Ingreso de la persona destino
+             System.out.println("Ingrese el ID de la persona destino: ");
+             finNodo= scan.nextInt() - 1;
+				
+             // Valores temporales en arrays
+             long[] nodos = {0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+             boolean[] temps ={null,null,null,null,null,null,null,null,null,null,null,null,null,null};
+             int[] desde = new int[14];
+			
+             nodos[indNodo]=0;
+             desde[indNodo]=indNodo;
+			
+             for(int t=0; t<14; t++){
+             for(int i=0; i<vectorID.size(); i++){
+			 Relationship relation = graphDb.getRelationshipById(vectorID.get(i));
+			 int nA= (int)relation.getStartNode().getId();
+			 int nB= (int)relation.getEndNode().getId();
+			 long l1= (long) relation.getProperty("Emails");
+                   
+			 // Se almacenan temporalmente las relaciones entre los nodos
+			 if(nA==indNodo){
+			 if (nodos[nB]>(nodos[indNodo]+l1)){
+			 nodos[nB]=nodos[indNodo]+l1;
+			 desde[nB]=indNodo;
+			 }
+			 }
+			 }
+               
+			 // Se busca el menor de los nodos 
+			 long b= 10000;
+			 int  m =-1;
+			 for(int j=0; j<nodos.length; j++){
+			 long d= nodos[j];
+			 if(temps[j]==null){
+			 if (d<=b){
+			 b=d;
+			 m=j;
+			 }
+			 }
+             }
+			 temps[m]=true;
+			 indNodo=m;						
+             } 
+         
+
+			 // Se busca para todos los nodos
+             if (finNodo==-1){
+			 for (int i=0; i<14; i++){
+			 long nds= nodos[i];
+			 if(nds==0)
+			 System.out.println("La persona "+(iNodo+1)+" no ha mandado correos a la persona "+(i+1));
+			 else	
+			 System.out.println("La cantidad minima de emails enviados de la persona "+(iNodo+1)+" a la persona"+(i+1)+" son: "+nds);
+             }
+             }else{
+
+			 // Dado un nodo destino 
+			 long ndc=nodos[finNodo];
+                
+			 // Si la relacion nunca fue modificada
+			 if(ndc==0)
+			 System.out.println("La persona "+(iNodo+1)+" no ha mandado correos a la persona "+(finNodo+1));
+			 else
+			 System.out.println("La cantidad minima de correos enviados por la persona "+(iNodo+1)+" a la persona"+(finNodo+1)+" son: "+ndc);
+             }
+    
 			 tx.success();
-			 
 		 }
 		 
 		 dbService.shutdown(); //se apaga la base de datos
